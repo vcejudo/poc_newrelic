d836f4d6a29ed44198356dbe29a14c248c517f9d
Victor Cejudo <victor.cejudo@awaze.com>
2022-09-28 11:18:09 +0100

    Send many NewRelic events

--- src/main.clj
+++ src/main.clj
@@ -3,9 +3,15 @@
   (:import (com.newrelic.api.agent NewRelic)))
 
 (defn- record-event [data-to-record]
-  (.. (NewRelic/getAgent)
-      (getInsights)
-      (recordCustomEvent "MyCustomEvent" data-to-record)))
+  (loop [x 1]
+    (when (< x 100)
+      (let [data-to-send (assoc data-to-record "iteration" (str x))]
+        (println "Sending>> " data-to-send)
+        (.. (NewRelic/getAgent)
+            (getInsights)
+            (recordCustomEvent "MyCustomEvent" data-to-send))
+        (Thread/sleep 2000)
+        (recur (inc x))))))
 
 (defn -main []
   (println "Hello, World!")
